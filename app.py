fab428c77c1a4744eb4287cc00f912c652baa9a7
RegiReis7 <reginaldo@live.com.pt>
2024-01-10 01:29:13 -0300

    Embeddings

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
 import streamlit as st
+from dotenv import load_dotenv
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import openai, huggingface_hub
+from langchain.vectorstores import faiss
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap = 200,
+        length_function=len
+    )
+
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = huggingface_hub.HuggingFaceHubEmbeddings(model="hkunlp/instructor-xl")
+    vectorstore = faiss.FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Chat With Multiple PDFs", page_icon=":books:")
 
     st.header("Chat with multiple PDFs :books:")
@@ -9,8 +41,14 @@ def main():
 
     with st.sidebar:
         st.subheader("Your Documents")
-        st.file_uploader("Upload your PDFs here and click on \'Proccess\'")
-        st.button("Proccess")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on \'Proccess\'", accept_multiple_files=True)
+        if st.button("Proccess"):
+            with st.spinner("Processing"):
+                raw_text = get_pdf_text(pdf_docs)
+
+                text_chunks = get_text_chunks(raw_text)
+                
+                vectorstore = get_vectorstore(text_chunks)
 
 
 if __name__ == '__main__':
